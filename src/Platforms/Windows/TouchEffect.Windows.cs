ca95a2f1b25b2733a3e9ec51e006bee1dcdb2cb0
Nick Kovalsky <nickyboom@mail.ru>
2023-09-03 11:02:31 +0300

    count fingers differently

--- src/Platforms/Windows/TouchEffect.Windows.cs
+++ src/Platforms/Windows/TouchEffect.Windows.cs
@@ -5,123 +5,124 @@ using System.ComponentModel;
 
 namespace AppoMobi.Maui.Gestures
 {
-	public partial class PlatformTouchEffect : PlatformEffect
-	{
-		FrameworkElement frameworkElement;
+    public partial class PlatformTouchEffect : PlatformEffect
+    {
+        FrameworkElement frameworkElement;
 
 
-		protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
-		{
-			base.OnElementPropertyChanged(args);
+        protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
+        {
+            base.OnElementPropertyChanged(args);
 
-			if (args.PropertyName == "Window" && Element is View view)
-			{
-				//if (view.Window == null)
-				//    OnDetached();
-			}
-		}
-		protected override void OnAttached()
-		{
-			// Get the Windows FrameworkElement corresponding to the Element that the effect is attached to
-			frameworkElement = Control == null ? Container : Control;
+            if (args.PropertyName == "Window" && Element is View view)
+            {
+                //if (view.Window == null)
+                //    OnDetached();
+            }
+        }
+        protected override void OnAttached()
+        {
+            // Get the Windows FrameworkElement corresponding to the Element that the effect is attached to
+            frameworkElement = Control == null ? Container : Control;
 
-			// Get access to the TouchEffect class in the .NET Standard library
-			_touchEffect = Element.Effects.OfType<TouchEffect>().FirstOrDefault();
+            // Get access to the TouchEffect class in the .NET Standard library
+            _touchEffect = Element.Effects.OfType<TouchEffect>().FirstOrDefault();
 
-			if (_touchEffect != null && frameworkElement != null)
-			{
-				// Save the method to call on touch events
+            if (_touchEffect != null && frameworkElement != null)
+            {
+                // Save the method to call on touch events
 
-				// Set event handlers on FrameworkElement
-				frameworkElement.PointerPressed += OnPointerPressed;
-				frameworkElement.PointerMoved += OnPointerMoved;
-				frameworkElement.PointerReleased += OnPointerReleased;
-				frameworkElement.PointerExited += OnPointerExited;
-			}
-		}
-
-
-		protected override void OnDetached()
-		{
-			if (frameworkElement != null)
-			{
-				frameworkElement.PointerPressed -= OnPointerPressed;
-				frameworkElement.PointerMoved -= OnPointerMoved;
-				frameworkElement.PointerReleased -= OnPointerReleased;
-				frameworkElement.PointerExited -= OnPointerExited;
-			}
-		}
-
-		private bool _pressed = false;
-
-		private volatile TouchEffect _touchEffect;
-
-		void OnPointerPressed(object sender, PointerRoutedEventArgs args)
-		{
-			_pressed = true;
-			FireEvent(sender, TouchActionType.Pressed, args);
-		}
-
-		void OnPointerMoved(object sender, PointerRoutedEventArgs args)
-		{
-			if (_pressed)
-				FireEvent(sender, TouchActionType.Moved, args);
-		}
-
-		void OnPointerReleased(object sender, PointerRoutedEventArgs args)
-		{
-			FireEvent(sender, TouchActionType.Released, args);
-			_pressed = false;
-		}
-
-		private void OnPointerExited(object sender, PointerRoutedEventArgs args)
-		{
-			if (_pressed)
-			{
-				_pressed = false;
-				FireEvent(sender, TouchActionType.Exited, args);
-			}
-		}
+                // Set event handlers on FrameworkElement
+                frameworkElement.PointerPressed += OnPointerPressed;
+                frameworkElement.PointerMoved += OnPointerMoved;
+                frameworkElement.PointerReleased += OnPointerReleased;
+                frameworkElement.PointerExited += OnPointerExited;
+            }
+        }
 
 
-		void FireEvent(object sender, TouchActionType touchActionType, PointerRoutedEventArgs pointer)
-		{
-			try
-			{
+        protected override void OnDetached()
+        {
+            if (frameworkElement != null)
+            {
+                frameworkElement.PointerPressed -= OnPointerPressed;
+                frameworkElement.PointerMoved -= OnPointerMoved;
+                frameworkElement.PointerReleased -= OnPointerReleased;
+                frameworkElement.PointerExited -= OnPointerExited;
+            }
+        }
 
-				var pointerPoint = pointer.GetCurrentPoint(sender as UIElement);
+        private bool _pressed = false;
 
-				var windowsPoint = pointerPoint.Position;
+        private volatile TouchEffect _touchEffect;
 
-				var args = new TouchActionEventArgs(
-					pointer.Pointer.PointerId,
-					touchActionType,
-					new Microsoft.Maui.Graphics.PointF((float)(windowsPoint.X * TouchEffect.Density), (float)(windowsPoint.Y * TouchEffect.Density)), null);
+        private readonly HashSet<uint> activePointerIds = new HashSet<uint>();
 
-				args.IsInsideView = _pressed;
+        void OnPointerPressed(object sender, PointerRoutedEventArgs args)
+        {
+            _pressed = true;
+            activePointerIds.Add(args.Pointer.PointerId);
+            FireEvent(sender, TouchActionType.Pressed, args);
+        }
 
-				if (pointer.Pointer.IsInContact)
-				{
-					var points = pointer.GetIntermediatePoints(null);
-					if (points != null)
-					{
-						args.NumberOfTouches = points.Count;
-					}
-				}
-				else
-				{
-					args.NumberOfTouches = 1; //last finger released, and it was 1
-				}
+        void OnPointerMoved(object sender, PointerRoutedEventArgs args)
+        {
+            if (_pressed)
+                FireEvent(sender, TouchActionType.Moved, args);
+        }
 
-				//Trace.WriteLine($"TouchEffect: {touchActionType} {args.Location.X}x{args.Location.Y} {args.NumberOfTouches}");
+        void OnPointerReleased(object sender, PointerRoutedEventArgs args)
+        {
+            activePointerIds.Remove(args.Pointer.PointerId);
+            FireEvent(sender, TouchActionType.Released, args);
+            _pressed = activePointerIds.Count > 0;
+        }
 
-				_touchEffect?.OnTouchAction(args);
-			}
-			catch (System.Exception ex)
-			{
-				Console.WriteLine(ex);
-			}
+        void OnPointerExited(object sender, PointerRoutedEventArgs args)
+        {
+            if (_pressed)
+            {
+                activePointerIds.Remove(args.Pointer.PointerId);
+                _pressed = activePointerIds.Count > 0;
+                FireEvent(sender, TouchActionType.Exited, args);
+            }
+        }
 
-		}
-	}
+
+        void FireEvent(object sender, TouchActionType touchActionType, PointerRoutedEventArgs pointer)
+        {
+            try
+            {
+
+                var pointerPoint = pointer.GetCurrentPoint(sender as UIElement);
+
+                var windowsPoint = pointerPoint.Position;
+
+                var args = new TouchActionEventArgs(
+                    pointer.Pointer.PointerId,
+                    touchActionType,
+                    new Microsoft.Maui.Graphics.PointF((float)(windowsPoint.X * TouchEffect.Density), (float)(windowsPoint.Y * TouchEffect.Density)), null);
+
+                args.IsInsideView = _pressed;
+
+                if (pointer.Pointer.IsInContact)
+                {
+                    args.NumberOfTouches = activePointerIds.Count;
+                }
+                else
+                {
+                    args.NumberOfTouches = 1; //last finger released, and it was 1
+                }
+
+                //Trace.WriteLine($"TouchEffect: {touchActionType} {args.Location.X}x{args.Location.Y} {args.NumberOfTouches}");
+
+                _touchEffect?.OnTouchAction(args);
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+        }
+    }
 }
